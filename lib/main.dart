bf343c31a0f64b56170cca62df71788d8cb93461
mohamed gamal <61595547+mohamedelbalooty@users.noreply.github.com>
2025-08-29 20:27:25 +0300

    initialize app localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:movie_app/src/movie_app.dart';
 import 'package:movie_app/src/service_locator.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initServiceLocator();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(const MovieApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      startLocale: const Locale('en'),
+      saveLocale: true,
+      child: const MovieApp(),
+    ),
+  );
 }
